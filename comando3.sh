1c598779f574141594ce38e16db7b3afff847536
Your Name <you@example.com>
2024-08-12 18:41:37 -0300

    fix

--- comando3.sh
+++ comando3.sh
@@ -4,7 +4,8 @@ BASE_DIR="$(cd "$(dirname "$0")"; pwd)" || exit 2
 
 "$BASE_DIR/relay.sh" 2200
 
-play -q "$BASE_DIR/ui_alarm_01.mp3" -b 16 > /dev/null 2>&1
+# Ejecutar la alarma en segundo plano
+play -q "$BASE_DIR/ui_alarm_01.mp3" -b 16 > /dev/null 2>&1 &
 
 play -q "$BASE_DIR/ui_hacking_charscroll.wav" -b 16 > /dev/null 2>&1
 echo "Iniciando secuencia de autodestrucción..." | pv -qL 20
@@ -31,4 +32,4 @@ echo -n -e "\r[###################] 100% Proceso completado." | pv -qL 10
 sleep 1
 
 play -q "$BASE_DIR/ui_hacking_passbad.wav" -b 16 > /dev/null 2>&1
-echo -e "\n\nError: Secuencia de autodestrucción no autorizada. Abortando operación. Esta acción sera reportada" | pv -qL 20
+echo -e "\n\nError: Secuencia de autodestrucción no autorizada. Abortando operación. Esta acción será reportada" | pv -qL 20
