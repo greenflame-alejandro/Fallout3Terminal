636a22b497ade6b014d9cfb8c243c6a3b055db48
Your Name <you@example.com>
2024-09-10 17:04:41 -0300

    asd

--- run.sh
+++ run.sh
@@ -1,2 +1,1 @@
-cool-retro-term --fullscreen --geometry 640x480 --font-color "#00FF00" --background-color "#000000" --font "TERMINUS_SCALED" --brightness 0.5 --contrast 0.85 --bloom 0.4 --opacity 1 -e bash /home/vault/Escritorio/Fallout3Terminal/terminalscript
-#/Applications/cool-retro-term.app/Contents/MacOS/cool-retro-term -e bash /Users/palmera/Projects/own/Fallout3Terminal/terminalscript   
+cool-retro-term --fullscreen --noclose --profile /home/vault/Escritorio/Fallout3Terminal/terminal.json -e bash /home/vault/Escritorio/Fallout3Terminal/terminalscript
