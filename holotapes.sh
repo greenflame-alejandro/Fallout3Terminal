0d0c44c4f43b18ab4f08bf5293b1d90526aa0ea8
Your Name <you@example.com>
2024-09-17 09:52:00 -0300

    asd

--- holotapes.sh
+++ holotapes.sh
@@ -1,40 +1,58 @@
 #!/usr/bin/env bash
 
-holotape_reader_func() {
-    # Mostrar el texto del Menú
+BASE_DIR="$(cd "$(dirname "$0")"; pwd)" || exit 2
+
+# Función para reproducir sonidos
+play_sound() {
+    play -q "$BASE_DIR/$1" -b 16 > /dev/null 2>&1
+}
+
+# Función para mostrar el encabezado superior alineado al centro
+display_center(){
+    columns="$(tput cols)"
+    while IFS= read -r line; do
+        printf "%*s\n" $(( (${#line} + columns) / 2)) "$line"
+    done < "$1"
+}
+
+# Función para mostrar la fecha y hora con el año ajustado
+display_date_time() {
+    local current_date=$(date +"%Y-%m-%d %H:%M:%S")
+    local adjusted_year=2190
+    local adjusted_date="${adjusted_year}${current_date:4}"
+    printf "%*s\n" $(( (${#adjusted_date} + $(tput cols)) / 2)) "$adjusted_date"
+}
+
+# Aquí la función principal holotape_reader_func
+holotape_reader_func () {
     clear
     display_center "$BASE_DIR/greeterheader.txt"
-    display_date_time  # Mostrar la fecha y hora ajustada debajo del greeter
-    echo "$greeter" 
+    display_date_time  # Mostrar la fecha y hora ajustada
+    echo "$greeter"
     play_sound "ui_hacking_charscroll.wav"
     echo "Presiona CTRL+C para salir." | pv -qL 80
     echo
     play_sound "ui_hacking_charscroll.wav"
 
-    # Permitir la escritura libre sin guardar, validando caracteres y manejando líneas largas
     while true; do
         read -p "> " input_text
-        # Validar caracteres especiales
         valid_text=$(echo "$input_text" | tr -cd '[:alnum:] [:punct:]')
-        
-        # Si la longitud de la línea es demasiado larga, cortarla
+
         while [ ${#valid_text} -gt $(tput cols) ]; do
             echo "${valid_text:0:$(tput cols)}"
             valid_text="${valid_text:$(tput cols)}"
         done
-        
-        # Imprimir el texto restante
+
         echo "$valid_text"
-        
-        # Si el usuario presiona Ctrl+C, salir del bucle y volver al menú principal
+
         if [ -z "$input_text" ]; then
             break
         fi
     done
 
-    # Volver al menú principal
     play_sound "ui_hacking_charenter_01.wav"
-    repeatmainmenufunc
+    # Llamar a la función que quieras al finalizar
 }
 
+# Ejecutar la función automáticamente al llamar el script
 holotape_reader_func
